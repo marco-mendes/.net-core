a7e9d43d365d7db6a1229abbb47e4ae2fcef1788
Arthur <arthurgomesfaria@gmail.com>
2019-05-29 02:29:55 -0300

    Adicionado gabarito de xUnit

---
+++ NET Core/NET Core 2.2/xUnit/Terminal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Prime.Services;
+
+namespace Terminal
+{
+    public class Program {
+
+        static void Main(string[] args)
+        {
+            var _primeService = new PrimeService();
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while(max == -1 || counter < max)
+            {
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                Console.WriteLine($"Is Prime: {_primeService.IsPrime(counter)}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
