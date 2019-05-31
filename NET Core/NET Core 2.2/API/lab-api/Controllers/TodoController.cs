2d0d43f0a1685820e8e634d0ecb969c9ce44248f
Arthur Faria <arthurgomesfaria@gmail.com>
2019-05-31 04:21:17 -0300

    Update TodoController.cs

--- NET Core/NET Core 2.2/API/lab-api/Controllers/TodoController.cs
+++ NET Core/NET Core 2.2/API/lab-api/Controllers/TodoController.cs
@@ -54,5 +54,36 @@ namespace lab_api.Controllers
 
             return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
         }
+                // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
